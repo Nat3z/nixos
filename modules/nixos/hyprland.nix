5556a371e00c82a4c3d767d372e741eda092dad5
Nat3z <66748576+Nat3z@users.noreply.github.com>
2024-11-09 18:02:16 -0800

    Rebuild: default Sat Nov  9 06:02:16 PM PST 2024

--- modules/nixos/hyprland.nix
+++ modules/nixos/hyprland.nix
@@ -11,6 +11,11 @@ in
       useWofi = lib.mkEnableOption "Use Wofi";  
       useHyprPaper = lib.mkEnableOption "Use HyprPaper";
       useHyprlock = lib.mkEnableOption "Use Hyprlock";
+      autoLayout = lib.mkOption {
+        type = types.bool;
+        default = true;
+        description = "Hyprland Auto Layout Script";
+      };
       defaultSession = lib.mkOption {
         type = types.bool;
         default = true;
@@ -37,6 +42,31 @@ in
 
     (lib.mkIf cfg.defaultSession {
       services.displayManager.defaultSession = "hyprland";
+    })
+
+    (lib.mkIf cfg.autoLayout {
+      environment.systemPackages = [
+        pkgs.jq
+        (pkgs.writeShellScriptBin "hypr-layout" ''
+          # Function to move windows by class to a specified workspace
+          move_windows_to_workspace() {
+            local WINDOW_CLASS=$1
+            local WORKSPACE_ID=$2
+
+            # Get the PIDs of windows with the specified class and move them to the workspace
+            for pid in $(hyprctl -j clients | jq -r ".[] | select(.class == \"$WINDOW_CLASS\") | .pid")
+            do
+              hyprctl dispatch movetoworkspacesilent $WORKSPACE_ID,pid:$pid
+            done
+          }
+
+          move_windows_to_workspace 'vesktop' 1
+          move_windows_to_workspace 'zen-alpha' 3
+          move_windows_to_workspace 'org.vinegarhq.Sober' 2
+          move_windows_to_workspace 'Code' 4
+          move_windows_to_workspace 'steam' 5
+        '')
+      ];
     })
 
   ]);
