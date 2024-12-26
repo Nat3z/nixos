427992b34249a970b38799591c276e05e025f31e
Nat3z <66748576+Nat3z@users.noreply.github.com>
2024-12-25 22:52:21 -0800

    Rebuild: darwin Wed Dec 25 22:52:21 PST 2024

--- modules/home-manager/tmux.nix
+++ modules/home-manager/tmux.nix
@@ -27,7 +27,7 @@
 
     extraConfig = ''
       # https://old.reddit.com/r/tmux/comments/mesrci/tmux_2_doesnt_seem_to_use_256_colors/
-      set-option -g default-shell /bin/zsh
+      set -g default-shell /bin/zsh
       set -g default-terminal "xterm-256color"
       set -ga terminal-overrides ",*256col*:Tc"
       set -ga terminal-overrides '*:Ss=\E[%p1%d q:Se=\E[ q'
