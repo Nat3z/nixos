d379d8e9e56c4eacaaa8971e207ab2a7938f44c2
Nat3z <66748576+Nat3z@users.noreply.github.com>
2024-12-26 10:50:27 -0800

    Rebuild: darwin Thu Dec 26 10:50:27 PST 2024

--- modules/home-manager/tmux.nix
+++ modules/home-manager/tmux.nix
@@ -62,7 +62,7 @@
       bind-key -T copy-mode-vi 'C-k' select-pane -U
       bind-key -T copy-mode-vi 'C-l' select-pane -R
       bind-key -T copy-mode-vi 'C-\' select-pane -l
-      bind-key -n 'C-Left' if-shell "$is_vim" 'display-message "is vim"' 'display-message "is NOT vim"'
+      set-option -g default-command "reattach-to-user-namespace -l zsh"
     '';
   };
 }
