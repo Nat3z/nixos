ae27ed1a7bbadd1a8710a5927009ced2b013269e
Nat3z <66748576+Nat3z@users.noreply.github.com>
2024-12-26 10:53:33 -0800

    Rebuild: darwin Thu Dec 26 10:53:33 PST 2024

--- modules/home-manager/tmux.nix
+++ modules/home-manager/tmux.nix
@@ -27,7 +27,7 @@
 
     extraConfig = ''
       # https://old.reddit.com/r/tmux/comments/mesrci/tmux_2_doesnt_seem_to_use_256_colors/
-      set -g default-shell /bin/zsh
+      set -g default-shell $SHELL
       set -g default-terminal "xterm-256color"
       set -ga terminal-overrides ",*256col*:Tc"
       set -ga terminal-overrides '*:Ss=\E[%p1%d q:Se=\E[ q'
@@ -62,7 +62,6 @@
       bind-key -T copy-mode-vi 'C-k' select-pane -U
       bind-key -T copy-mode-vi 'C-l' select-pane -R
       bind-key -T copy-mode-vi 'C-\' select-pane -l
-      set-option -g default-command "reattach-to-user-namespace -l zsh"
     '';
   };
 }
