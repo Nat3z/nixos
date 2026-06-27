fa2bad4c36d3f2694e60a3037dfcf60babf3027b
Nat3z <66748576+Nat3z@users.noreply.github.com>
2026-06-27 19:32:28 +0800

    Rebuild: darwin Sat Jun 27 19:32:28 PST 2026

--- modules/home-manager/tmux.nix
+++ modules/home-manager/tmux.nix
@@ -22,8 +22,8 @@
         plugin = tmuxPlugins.catppuccin;
         extraConfig = ''
           set -g @catppuccin_flavor 'mocha'
-          set -g @catppuccin_window_tabs_enabled on
-          set -g @catppuccin_date_time "%m-%d-%Y %I:%M %p"
+          set -g @catppuccin_window_status_style 'rounded'
+          set -g @catppuccin_date_time_text ' %m-%d-%Y %I:%M %p'
         '';
       }
       tmuxPlugins.yank
@@ -33,10 +33,22 @@
       # https://old.reddit.com/r/tmux/comments/mesrci/tmux_2_doesnt_seem_to_use_256_colors/
       set -g default-shell $SHELL
       set -g default-command $SHELL
-      set -g default-terminal "xterm-256color"
+      set -g default-terminal "tmux-256color"
       set -ga terminal-overrides ",*256col*:Tc"
       set -ga terminal-overrides '*:Ss=\E[%p1%d q:Se=\E[ q'
       set-environment -g COLORTERM "truecolor"
+
+      # Catppuccin v2 only defines status modules by default; opt into using them.
+      # Keep the status bar visible as a full-width bar at the bottom.
+      set -g status on
+      set -g status-position bottom
+      set -gF status-style "bg=#{@thm_mantle},fg=#{@thm_fg}"
+      set -g status-right-length 100
+      set -g status-left-length 100
+      set -g status-left ""
+      set -g status-right "#{E:@catppuccin_status_application}"
+      set -ag status-right "#{E:@catppuccin_status_session}"
+      set -ag status-right "#{E:@catppuccin_status_date_time}"
 
       # Mouse works as expected
       set-option -g mouse on
@@ -49,24 +61,23 @@
 
       bind-key -T copy-mode-vi v send-keys -X begin-selection
       bind-key -T copy-mode-vi C-v send-keys -X rectangle-toggle
-      bind-key -T copy-mode-vi y send-keys -X copy-pipe-and-cancel 
-      is_vim="ps -t '#{pane_tty}' | grep nvim"
-      bind-key -n 'C-h' if-shell "$is_vim" 'send-keys C-h'  'select-pane -L'
-      bind-key -n 'C-j' if-shell "$is_vim" 'send-keys C-j'  'select-pane -D'
-      bind-key -n 'C-k' if-shell "$is_vim" 'send-keys C-k'  'select-pane -U'
-      bind-key -n 'C-l' if-shell "$is_vim" 'send-keys C-l'  'select-pane -R'
-      tmux_version='$(tmux -V | sed -En "s/^tmux ([0-9]+(.[0-9]+)?).*/\1/p")'
-      if-shell -b '[ "$(echo "$tmux_version < 3.0" | bc)" = 1 ]' \
-          "bind-key -n 'C-\\' if-shell \"$is_vim\" 'send-keys C-\\'  'select-pane -l'"
-      if-shell -b '[ "$(echo "$tmux_version >= 3.0" | bc)" = 1 ]' \
-          "bind-key -n 'C-\\' if-shell \"$is_vim\" 'send-keys C-\\\\'  'select-pane -l'"
+      bind-key -T copy-mode-vi y send-keys -X copy-pipe-and-cancel
+
+      # vim/tmux navigation.  Use pane_current_command instead of shell vars in
+      # tmux.conf; the old is_vim/tmux_version assignments were not reliable tmux
+      # commands, and the C-\\ binding was not getting installed.
+      bind-key -n 'C-h' if -F '#{m/r:(^|/)(g?view|n?vim?x?)(diff)?$,#{pane_current_command}}' 'send-keys C-h'  'select-pane -L'
+      bind-key -n 'C-j' if -F '#{m/r:(^|/)(g?view|n?vim?x?)(diff)?$,#{pane_current_command}}' 'send-keys C-j'  'select-pane -D'
+      bind-key -n 'C-k' if -F '#{m/r:(^|/)(g?view|n?vim?x?)(diff)?$,#{pane_current_command}}' 'send-keys C-k'  'select-pane -U'
+      bind-key -n 'C-l' if -F '#{m/r:(^|/)(g?view|n?vim?x?)(diff)?$,#{pane_current_command}}' 'send-keys C-l'  'select-pane -R'
+      bind-key -n 'C-\\' if -F '#{m/r:(^|/)(g?view|n?vim?x?)(diff)?$,#{pane_current_command}}' 'send-keys C-\\' 'select-pane -l'
 
       bind-key -T copy-mode-vi 'C-h' select-pane -L
       bind-key -T copy-mode-vi 'C-j' select-pane -D
       bind-key -T copy-mode-vi 'C-k' select-pane -U
       bind-key -T copy-mode-vi 'C-l' select-pane -R
       bind-key -T copy-mode-vi 'C-\' select-pane -l
-      bind-key -n 'C-g' if-shell "$is_vim" 'display-message "is vim"' 'display-message "is NOT vim"'
+      bind-key -n 'C-g' if -F '#{m/r:(^|/)(g?view|n?vim?x?)(diff)?$,#{pane_current_command}}' 'display-message "is vim"' 'display-message "is NOT vim"'
     '';
   };
 }
